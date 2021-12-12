0ee045e5c6fa89ae656d3dbb0d6ef2b0a0a6208d
Brian Carrigan <brian@bcarrigan.com>
2021-12-12 07:53:52 -0500

    Day 12 Part 2

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -6,7 +6,11 @@ struct Edge {
     point_two: String
 }
 
-type Path = Vec<String>;
+
+struct Path {
+    nodes: Vec<String>,
+    small_visited_twice: bool
+}
 
 fn read_input(path: &str) -> Vec<Edge> {
     fs::read_to_string(path)
@@ -26,8 +30,16 @@ fn is_small_cave(cave: &str) -> bool {
     cave.chars().all(|c| match c { 'a'..='z' => true, _ => false })
 }
 
-fn permutate_path(edges: &Vec<Edge>, path: &Path) -> Vec<Path> {
-    let last_visit = path.last().unwrap();
+fn validate_path(node: &String, path: &Path, can_visit_small_cave_twice: bool) -> bool {
+    if node == "start" { return false; }
+    if !is_small_cave(node) { return true; }
+    if can_visit_small_cave_twice && !path.small_visited_twice { return true; }
+
+    !path.nodes.contains(node)
+}
+
+fn permutate_path(edges: &Vec<Edge>, path: &Path, can_visit_small_cave_twice: bool) -> Vec<Path> {
+    let last_visit = path.nodes.last().unwrap();
 
     edges
         .iter()
@@ -38,27 +50,29 @@ fn permutate_path(edges: &Vec<Edge>, path: &Path) -> Vec<Path> {
                 _ => None
             }
         })
-        .filter(|node| !is_small_cave(node) || !path.contains(node))
+        .filter(|node| validate_path(node, path, can_visit_small_cave_twice))
         .map(|node| {
-            let mut new_path = path.clone();
+            let mut new_path = path.nodes.clone();
             new_path.push(node.clone());
 
-            new_path
+            let small_visited_twice = is_small_cave(node) && new_path.iter().filter(|&n| n == node).count() == 2;
+
+            Path { nodes: new_path, small_visited_twice: path.small_visited_twice || small_visited_twice }
         })
         .collect()
 }
 
-fn part_one(edges: &Vec<Edge>) -> usize {
-    let initial_path = vec!("start".to_string());
+fn traverse_cave(edges: &Vec<Edge>, can_visit_small_cave_twice: bool) -> usize {
+    let initial_path = Path { nodes: vec!("start".to_string()), small_visited_twice: false };
     let mut paths_in_progress = vec!(initial_path);
     let mut finished_paths = Vec::new();
 
     while paths_in_progress.len() > 0 {
         let path = paths_in_progress.remove(0);
-        let permutations = permutate_path(edges, &path);
+        let permutations = permutate_path(edges, &path, can_visit_small_cave_twice);
 
         for permutation in permutations {
-            match permutation.last().unwrap().as_str() {
+            match permutation.nodes.last().unwrap().as_str() {
                 "end" => finished_paths.push(permutation),
                 _ => paths_in_progress.push(permutation)
             }
@@ -68,9 +82,18 @@ fn part_one(edges: &Vec<Edge>) -> usize {
     finished_paths.len()
 }
 
+fn part_one(edges: &Vec<Edge>) -> usize {
+    traverse_cave(edges, false)
+}
+
+fn part_two(edges: &Vec<Edge>) -> usize {
+    traverse_cave(edges, true)
+}
+
 fn main() {
     let edges = read_input("input");
     println!("Day 12 Part 1: {}", part_one(&edges));
+    println!("Day 12 Part 2: {}", part_two(&edges));
 }
 
 #[test]
@@ -83,4 +106,16 @@ fn test_part_one() {
 
     let edges = read_input("test3");
     assert_eq!(part_one(&edges), 226);
+}
+
+#[test]
+fn test_part_two() {
+    let edges = read_input("test1");
+    assert_eq!(part_two(&edges), 36);
+
+    let edges = read_input("test2");
+    assert_eq!(part_two(&edges), 103);
+
+    let edges = read_input("test3");
+    assert_eq!(part_two(&edges), 3509);
 }
