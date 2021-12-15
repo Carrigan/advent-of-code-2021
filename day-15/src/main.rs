616e77f1d63302c8521444dfe19d227f32c32fef
Brian Carrigan <brian@bcarrigan.com>
2021-12-15 11:42:11 -0500

    Day 15 Part 1: Dijkstra's algorithm

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,27 +1,5 @@
 use std::fs;
 
-struct VisitedNode {
-    index: usize,
-    parent: Option<usize>,
-    total_cost: usize
-}
-
-struct VisitedNodeIterator<'a> {
-    nodes: &'a Vec<VisitedNode>,
-    index: Option<usize>
-}
-
-impl <'a> Iterator for VisitedNodeIterator<'a> {
-    type Item = usize;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let node = &self.nodes[self.index?];
-        self.index = node.parent;
-
-        Some(node.index)
-    }
-}
-
 fn read_input(path: &str) -> Vec<usize> {
     fs::read_to_string(path)
         .expect("File path must be valid")
@@ -42,45 +20,37 @@ fn neighboring_indeces(index: usize, width: usize, size: usize) -> Vec<usize> {
 }
 
 fn part_one(map: &Vec<usize>, width: usize) -> usize {
-    let mut nodes = vec!(VisitedNode { index: 0, parent: None, total_cost: 0 });
-    let mut to_visit = vec!((0, 0)); // (node_index, node_cost)
     let map_size = map.len();
     let destination = map_size - 1;
+    let mut unvisited_indeces: Vec<usize> = (0..map_size).collect();
+    let mut tentative_distances: Vec<usize> = (0..map_size).map(|_| usize::MAX).collect();
+    tentative_distances[0] = 0;
 
-    while to_visit.len() > 0 {
-        let to_visit_index = to_visit
+    loop {
+        let (unvisited_index, &current_node_index) = unvisited_indeces
             .iter()
             .enumerate()
-            .min_by(|(_, (_, c1)), (_, (_, c2))| c1.cmp(c2))
-            .unwrap()
-            .0;
+            .min_by(|(_, &i1), (_, &i2)| {
+                tentative_distances[i1].cmp(&tentative_distances[i2])
+            })
+            .unwrap();
 
-        let lowest_cost_index = to_visit.remove(to_visit_index).0;
-        let lowest_cost = nodes[lowest_cost_index].total_cost;
+        unvisited_indeces.remove(unvisited_index);
 
-        for neighboring_map_index in neighboring_indeces(nodes[lowest_cost_index].index, width, map_size) {
-            let path_cost = lowest_cost + map[neighboring_map_index];
+        let current_node_cost = tentative_distances[current_node_index];
 
-            if neighboring_map_index == destination {
-                return path_cost;
+        for neighbor_index in neighboring_indeces(current_node_index, width, map_size) {
+            let neighbor_cost = current_node_cost + map[neighbor_index];
+
+            if neighbor_index == destination {
+                return neighbor_cost;
             }
 
-            let already_visited = VisitedNodeIterator { nodes: &nodes, index: Some(lowest_cost_index) }
-                .any(|node_map_index| node_map_index == neighboring_map_index);
-
-            if already_visited { continue; }
-
-            let insertion_index = nodes.len();
-            nodes.push(VisitedNode {
-                index: neighboring_map_index,
-                parent: Some(lowest_cost_index),
-                total_cost: path_cost
-            });
-            to_visit.push((insertion_index, path_cost));
+            if neighbor_cost < tentative_distances[neighbor_index] {
+                tentative_distances[neighbor_index] = neighbor_cost;
+            }
         }
     }
-
-    panic!("No solution was found")
 }
 
 fn main() {
